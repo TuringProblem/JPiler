e5c864b7b0eca9fe30ce34a250ed92b9fe86853c
TuringProblem <andrew.deca.wellington@gmail.com>
2025-04-17 01:02:39 -0400

    feat: started to build the structure for the application, added the ability to add code into the text box and added a button for EventListener to produce outcome data :)

--- src/core/MainWindow.java
+++ src/core/MainWindow.java
@@ -2,6 +2,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.FlowLayout;
+import javax.swing.JTextArea;
 
 public class MainWindow {
 
@@ -14,16 +15,33 @@ public class MainWindow {
   private void initialize() {
     frame = new JFrame("Java Transpiler");
     this.frame.setLayout(new BorderLayout());
-    this.frame.setSize(500, 500);
+    this.frame.setSize(500, 1000);
     this.frame.setLocationRelativeTo(null);
     this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
     JPanel panel = new JPanel(); // default a FlowLayout
     panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-    Button button = new Button("Click to print Text");
-    panel.add(button);
+    panel.setBackground(Color.lightGray);
+    
+    //JPanel cP = new JPanel(); // Center Panel
+    //cP.setLayout(new FlowLayout(FlowLayout.left, 5, 5));
+    frame.add(new JTextArea(), BorderLayout.CENTER);
 
-    frame.add(panel, BorderLayout.CENTER);
+
+    // Button component
+    Button buttonOne = new Button("Click to print Text");
+    //Button buttonTwo = new Button("Hi");
+
+    panel.add(buttonOne);
+    //panel.add(buttonTwo);
+
+
+    //prefered size
+    panel.setPreferredSize(new Dimension(250, 50)); // width, height
+
+
+    frame.add(panel, BorderLayout.SOUTH);
     this.frame.setVisible(true);
   }
 }
